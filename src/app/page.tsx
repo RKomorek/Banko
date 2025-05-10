9d2d8620e1e787ad48d0c0db785c03ffd4288513
Komorek <rafa.kaquino@gmail.com>
2025-05-10 16:09:39 -0300

    Fix: remocao de configs do mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 
-import { AppUser } from "@/types/user";
-import { useState, useEffect } from "react";
-
 export default function Home() {
-  const [users, setUsers] = useState<AppUser[]>([]);
-
-  useEffect(() => {
-    fetch("/api/bff/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
 
   return (
     <main className="p-4">
       <h1 className="text-2xl font-bold mb-4">Usuários</h1>
       <ul>
-        {users.map((user, index) => (
-          <li key={user.id + index}>👤 {user.name}</li>
-        ))}
       </ul>
     </main>
   );
