3f5e93bf6a663c0dd3f5bd5bdb31452968228132
Komorek <rafa.kaquino@gmail.com>
2025-05-14 20:23:27 -0300

    feat: adicao de exemplo de add usuario pela api

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,5 @@
 // Exemplo de uso
+import { IUser } from '@/interfaces/users.interfaces'
 import { supabase } from '../config/supabaseClient'
 
 export async function fetchUsers() {
@@ -14,3 +15,23 @@ export async function fetchUsers() {
   console.log('Usuários:', data)
   return { data }
 }
+
+
+// Exemplo de uso para adicionar um novo usuário
+export async function addUser(user: IUser) {
+  const { data, error } = await supabase
+    .from('users')
+    .insert(user)
+
+  if (error) {
+    console.error('Erro ao adicionar usuário:', error)
+    return { error }
+  }
+
+  console.log('Usuário adicionado:', data)
+  return { data }
+}
+// Exemplo de uso para adicionar um novo usuário pelo chat gpt
+// const { error } = await supabase
+//   .from('users')
+//   .insert([{ name: 'João', email: 'joao@email.com' }])
