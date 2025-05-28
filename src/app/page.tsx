2537e2d4dbe987ae823691eb2dc3249ced4456f8
LoracVitoria <carolvitoria1705@gmail.com>
2025-05-28 19:09:08 -0300

    add welcome card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { BalanceCard } from "@/components/ui/balance-card";
 import { CardPaymentMethod } from "@/components/ui/payment-method";
+import { WelcomeCard } from "@/components/ui/welcome-card";
 import { cn } from "@/lib/utils";
 
 
@@ -28,6 +29,9 @@ export default function Home() {
       <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
 
         <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+          <Container>
+            <WelcomeCard></WelcomeCard>
+          </Container>
           <Container>
             <BalanceCard></BalanceCard>
           </Container>
--- src/components/ui/balance-card.tsx
+++ src/components/ui/balance-card.tsx
@@ -26,37 +26,37 @@ const chartConfig = {
 
 export function BalanceCard() {
   return (
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle>Saldo</CardTitle>
-        </CardHeader>
-        <CardContent className="pb-0">
-          <div className="text-2xl font-bold">R$10.675,89</div>
-          <p className="text-xs text-muted-foreground">
-            +20.1% desde o último mês
-          </p>
-          <ChartContainer config={chartConfig} className="h-[80px] w-full">
-            <LineChart
-              data={data}
-              margin={{
-                top: 5,
-                right: 10,
-                left: 10,
-                bottom: 0,
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle>Saldo</CardTitle>
+      </CardHeader>
+      <CardContent className="pb-0">
+        <div className="text-2xl font-bold">R$10.675,89</div>
+        <p className="text-xs text-muted-foreground">
+          +20.1% desde o último mês
+        </p>
+        <ChartContainer config={chartConfig} className="h-[80px] w-full">
+          <LineChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <Line
+              type="monotone"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke="var(--primary)"
+              activeDot={{
+                r: 6,
               }}
-            >
-              <Line
-                type="monotone"
-                strokeWidth={2}
-                dataKey="revenue"
-                stroke="var(--primary)"
-                activeDot={{
-                  r: 6,
-                }}
-              />
-            </LineChart>
-          </ChartContainer>
-        </CardContent>
-      </Card>
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
   );
 }
