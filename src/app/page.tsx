bc6e9a12617ce13be42ec41b3cb0e75c119796e5
Marcelo Sabino <eu@marcelosabino.net>
2025-06-02 23:00:58 -0300

    fix: 🧱 corrige layout e estilo para manter sidebar fixa e responsiva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ function Container({
 export default function Home() {
   return (
     <main className="p-4">
-      <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
+      <div className=" items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
         <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
           <Container>
             <WelcomeCard />
